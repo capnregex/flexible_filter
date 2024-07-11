f4945f397af846ce3281212e4fd1ad1c0ca29c2b
Robert Ferney <rob@ferney.org>
2024-07-11 13:30:47 -0600

    article tag

--- dummy/db/migrate/20240711162033_create_article_tags.rb
+++ dummy/db/migrate/20240711162033_create_article_tags.rb
@@ -1,6 +1,6 @@
 class CreateArticleTags < ActiveRecord::Migration[7.1]
   def change
-    create_table :article_tags do |t|
+    create_table :article_tags, primary_key: [:article_id, :tag_id] do |t|
       t.belongs_to :article, null: false, foreign_key: true
       t.belongs_to :tag, null: false, foreign_key: true
 
--- dummy/db/schema.rb
+++ dummy/db/schema.rb
@@ -1,4 +1,4 @@
-e This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_11_162033) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "article_tags", force: :cascade do |t|
+  create_table "article_tags", primary_key: ["article_id", "tag_id"], force: :cascade do |t|
     t.bigint "article_id", null: false
     t.bigint "tag_id", null: false
     t.datetime "created_at", null: false
